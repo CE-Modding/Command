99beb7e61b27a281167984aacea5b33c99907ec8
Cal Eagle <cal97eagle@gmail.com>
2026-05-28 22:39:32 +0100

    Music Name corrections

--- Command_Audio/Music/CommandConquer_RenX.hpp
+++ Command_Audio/Music/CommandConquer_RenX.hpp
@@ -1,34 +1,34 @@
 class RenX_CommandConquer
 {
-	name="Command & Conquer";
+	name="[C&C Renegade X] Command & Conquer";
 	sound[]={"Command_Audio\Music\CommandConquer\RenX\RenX_CommandConquer.ogg",db+0,1};
 	duration=260;
 	musicClass="CommandConquer_RenX";
 };
 class RenX_AnotherPresent
 {
-	name="Another Present For Ya";
+	name="[C&C Renegade X] Another Present For Ya";
 	sound[]={"Command_Audio\Music\CommandConquer\RenX\RenX_AnotherPresent.ogg",db+0,1};
 	duration=174;
 	musicClass="CommandConquer_RenX";
 };
 class RenX_ActInstinct
 {
-	name="Act On Instinct";
+	name="[C&C Renegade X] Act On Instinct";
 	sound[]={"Command_Audio\Music\CommandConquer\RenX\RenX_ActInstinct.ogg",db+0,1};
 	duration=189;
 	musicClass="CommandConquer_RenX";
 };
 class RenX_RockRoll
 {
-	name="Rock 'n' Roll";
+	name="[C&C Renegade X] Rock 'n' Roll";
 	sound[]={"Command_Audio\Music\CommandConquer\RenX\RenX_RockRoll.ogg",db+0,1};
 	duration=99;
 	musicClass="CommandConquer_RenX";
 };
 class RenX_Rampage
 {
-	name="Rampage";
+	name="[C&C Renegade X] Rampage";
 	sound[]={"Command_Audio\Music\CommandConquer\RenX\RenX_Rampage.ogg",db+0,1};
 	duration=79;
 	musicClass="CommandConquer_RenX";
--- Command_Audio/Music/CommandConquer_Tiberium.hpp
+++ Command_Audio/Music/CommandConquer_Tiberium.hpp
@@ -1,34 +1,34 @@
 class JustDoItUp
 {
-	name="[Tiberian Sons] Just Do It Up";
+	name="[Tiberium Wars] Tiberian Sons - Just Do It Up";
 	sound[]={"Command_Audio\Music\CommandConquer\Tiberium\JustDoItUp.ogg",db+0,1};
 	duration=147;
 	musicClass="CommandConquer_Tiberium";
 };
 class UnstoppableForce
 {
-	name="Unstoppable Force";
+	name="[Tiberium Wars] Unstoppable Force";
 	sound[]={"Command_Audio\Music\CommandConquer\Tiberium\UnstoppableForce.ogg",db+0,1};
 	duration=66;
 	musicClass="CommandConquer_Tiberium";
 };
 class KillStreak
 {
-	name="Kill Streak";
+	name="[Tiberium Wars] Kill Streak";
 	sound[]={"Command_Audio\Music\CommandConquer\Tiberium\KillStreak.ogg",db+0,1};
 	duration=125;
 	musicClass="CommandConquer_Tiberium";
 };
 class ExtractionPoint
 {
-	name="Extraction Point";
+	name="[Tiberium Wars] Extraction Point";
 	sound[]={"Command_Audio\Music\CommandConquer\Tiberium\ExtractionPoint.ogg",db+0,1};
 	duration=95;
 	musicClass="CommandConquer_Tiberium";
 };
 class PlanetTerror
 {
-	name="Planet of Terror";
+	name="[Tiberium Wars] Planet of Terror";
 	sound[]={"Command_Audio\Music\CommandConquer\Tiberium\PlanetTerror.ogg",db+0,1};
 	duration=93;
 	musicClass="CommandConquer_Tiberium";
