b1df9e1f542fc8351db489e1d31a6ead388f54ae
Cal Eagle <cal97eagle@gmail.com>
2026-08-20 03:17:12 +0100

    Patches update.

--- Command_Master/config.cpp
+++ Command_Master/config.cpp
@@ -38,7 +38,8 @@ class CfgEditorSubcategories
 
 class CfgUnitInsignia
 {
-    // Invis Background Textures (_CA)
+    /*
+    // EXAMPLE
     class Command_Insignia 
     {
         displayName="[CMD] Command"; // Name displayed in Arsenal
@@ -46,111 +47,130 @@ class CfgUnitInsignia
         texture="Command_Master\Data\Patch_Command_CA.paa"; // Image path
         textureVehicle="Command_Master\Data\Patch_Command_CA.paa"; // Does nothing currently, reserved for future use
     };
-    class Gator_Insignia 
-    {
-        displayName="[CMD] Gator-8"; // Name displayed in Arsenal
-        author="Tb"; // Author displayed in Arsenal
-        texture="Command_Master\Data\Patch_Gator8_CA.paa"; // Image path
-        textureVehicle="Command_Master\Data\Patch_Gator8_CA.paa"; // Does nothing currently, reserved for future use
+    
+    // _CA.paa must be used on textures that have transparent backgrounds! 
+    // _CO.paa must be used on textures that have solid backgrounds!
+    */
+
+    class Command_Insignia
+     {
+        displayName="[CMD] Command";
+        author="Tb + CE_Modding";
+        texture="Command_Master\Data\Patch_Command_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Command_CA.paa";
     };
-    class Fox_Insignia
+    class Command_ElementC_Insignia: Command_Insignia
+    {
+        displayName="[CMD] Command Element (Circle)";
+        texture="Command_Master\Data\Patch_CommandCircle_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_CommandCircle_CA.paa";
+    };
+    class Command_ElementS_Insignia: Command_Insignia
+    {
+        displayName="[CMD] Command Element (Square)";
+        texture="Command_Master\Data\Patch_CommandSquare_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_CommandSquare_CA.paa";
+    };
+    class Gator_Insignia: Command_Insignia
+    {
+        displayName="[CMD] Gator-8";
+        texture="Command_Master\Data\Patch_Gator8_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Gator8_CA.paa";
+    };
+    class Fox_Insignia: Command_Insignia
     {
         displayName="[CMD] Fox-1";
-        author="Tb";
         texture="Command_Master\Data\Patch_Fox1_CA.paa";
         textureVehicle="Command_Master\Data\Patch_Fox1_CA.paa";
     };
-    class Angels_Insignia
+    class Bastion_Insignia: Command_Insignia
     {
-        displayName="[CMD] Angels";
-        author="Tb";
-        texture="Command_Master\Data\Patch_Angels_CA.paa";
-        textureVehicle="Command_Master\Data\Patch_Angels_CA.paa";
+        displayName="[CMD] Bastion";
+        texture="Command_Master\Data\Patch_Bastion_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Bastion_CA.paa";
     };
-    class Foehammer_Insignia
+    class Foehammer_Insignia: Command_Insignia
     {
         displayName="[CMD] Foehammer";
-        author="Tb";
         texture="Command_Master\Data\Patch_Foehammer_CA.paa";
         textureVehicle="Command_Master\Data\Patch_Foehammer_CA.paa";
     };
-    class FU_Insignia
+    class Haven_Insignia: Command_Insignia
+    {
+        displayName="[CMD] Foehammer";
+        texture="Command_Master\Data\Patch_Foehammer_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Foehammer_CA.paa";
+    };
+    class Angels_Insignia: Command_Insignia
+    {
+        displayName="[CMD] Haven";
+        texture="Command_Master\Data\Patch_Haven_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Haven_CA.paa";
+    };
+    class AngelsLead_Insignia: Command_Insignia
+    {
+        displayName="[CMD] Angels Lead";
+        texture="Command_Master\Data\Patch_AngelsLead_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_AngelsLead_CA.paa";
+    };
+    class FU_Insignia: Command_Insignia
     {
         displayName="[CMD] SCP / Fuck You";
-        author="Cods";
+        author="Cods + CE_Modding";
         texture="Command_Master\Data\Patch_FU_CA.paa";
         textureVehicle="Command_Master\Data\Patch_FU_CA.paa";
     };
-    class AntiFurry_Insignia
+    class AntiFurry_Insignia: Command_Insignia
     {
         displayName="[CMD] Anti-Furry";
-        author="Andre";
+        author="Andre + CE_Modding";
         texture="Command_Master\Data\Patch_AntiFurry_CA.paa";
         textureVehicle="Command_Master\Data\Patch_AntiFurry_CA.paa";
     };
-    class Thankless_Insignia
+    class Thankless_Insignia: Command_Insignia
     {
         displayName="[CMD] It's a thankless job";
-        author="Gabriel Gilbert";
+        author="Gabriel Gilbert + CE_Modding";
         texture="Command_Master\Data\Patch_Thankless_CA.paa";
         textureVehicle="Command_Master\Data\Patch_Thankless_CA.paa";
     };
-    class DaWae_Insignia
+    class DaWae_Insignia: Command_Insignia
     {
         displayName="[CMD] Do you know da wae?";
-        author="???";
+        author="CE_Modding";
         texture="Command_Master\Data\Patch_DaWae_CA.paa";
         textureVehicle="Command_Master\Data\Patch_DaWae_CA.paa";
     };
-    class Cowabunga_Insignia
+    class Cowabunga_Insignia: Command_Insignia
     {
         displayName="[CMD] Cowabunga it is";
-        author="???";
+       author="CE_Modding";
         texture="Command_Master\Data\Patch_Cowabunga_CA.paa";
         textureVehicle="Command_Master\Data\Patch_Cowabunga_CA.paa";
     };
-
-    // Patch Background Textures (_CO)
-    class CommandAlt_Insignia
-    {
-        displayName="[CMD] Command (Alt)";
-        author="Tb";
-        texture="Command_Master\Data\Patch_CommandAlt_CO.paa";
-        textureVehicle="Command_Master\Data\Patch_CommandAlt_CO.paa";
-    };
-    class AngelsAlt_Insignia
-    {
-        displayName="[CMD] Angels (Alt)";
-        author="Tb";
-        texture="Command_Master\Data\Patch_AngelsAlt_CO.paa";
-        textureVehicle="Command_Master\Data\Patch_AngelsAlt_CO.paa";
-    };
-    class GovGetOut_Insignia
+    class GovGetOut_Insignia: Command_Insignia
     {
         displayName="[CMD] Government Get Out";
-        author="Theplahunter";
-        texture="Command_Master\Data\Patch_GovGetOut_CO.paa";
-        textureVehicle="Command_Master\Data\Patch_GovGetOut_CO.paa";
+        author="Theplahunter + CE_Modding";
+        texture="Command_Master\Data\Patch_GovGetOut_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_GovGetOut_CA.paa";
     };
-    class Yeeteth_Insignia
+    class Yeeteth_Insignia: Command_Insignia
     {
         displayName="[CMD] Lord Yeeteth";
-        author="Tb";
-        texture="Command_Master\Data\Patch_Yeeteth_CO.paa";
-        textureVehicle="Command_Master\Data\Patch_Yeeteth_CO.paa";
+        texture="Command_Master\Data\Patch_Yeeteth_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Yeeteth_CA.paa";
     };
-    class MTF_Insignia
+    class MTF_Insignia: Command_Insignia
     {
         displayName="[CMD] MTF";
-        author="Tb";
-        texture="Command_Master\Data\Patch_MTF_CO.paa";
-        textureVehicle="Command_Master\Data\Patch_MTF_CO.paa";
+        texture="Command_Master\Data\Patch_MTF_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_MTF_CA.paa";
     };
-    class Krill_Insignia
+    class Krill_Insignia: Command_Insignia
     {
         displayName="[CMD] Krill Yourself";
-        author="Tb";
-        texture="Command_Master\Data\Patch_Krill_CO.paa";
-        textureVehicle="Command_Master\Data\Patch_Krill_CO.paa";
+        texture="Command_Master\Data\Patch_Krill_CA.paa";
+        textureVehicle="Command_Master\Data\Patch_Krill_CA.paa";
     };
 };  
