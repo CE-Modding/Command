c52d71565f6eaf945fedfb015940a33b98c0b3ad
Cal Eagle <cal97eagle@gmail.com>
2026-09-09 15:47:01 +0100

    Added 12 more C&C TW tracks.

--- Command_Audio/Music/CommandConquer_Tiberium.hpp
+++ Command_Audio/Music/CommandConquer_Tiberium.hpp
@@ -33,3 +33,87 @@ class PlanetTerror
 	duration=93;
 	musicClass="CommandConquer_Tiberium";
 };
+class ToBeFeared
+{	
+	name="[Tiberium Wars] To Be Feared";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\ToBeFeared.ogg",db+0,1};
+	duration=267;
+	musicClass="CommandConquer_Tiberium";
+};
+class HellMarch_Remix
+{	
+	name="[Tiberium Wars] Hell March (Remix)";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\HellMarch_Remix.ogg",db+0,1};
+	duration=199.2;
+	musicClass="CommandConquer_Tiberium";
+};
+class ActonInstinct
+{	
+	name="[Tiberium Wars] Act on Instinct";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\ActonInstinct.ogg",db+0,1};
+	duration=151.2;
+	musicClass="CommandConquer_Tiberium";
+};
+class BigFoot
+{	
+	name="[Tiberium Wars] Big Foot";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\BigFoot.ogg",db+0,1};
+	duration=309;
+	musicClass="CommandConquer_Tiberium";
+};
+class BlowitUp
+{	
+	name="[Tiberium Wars] Blow it Up";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\BlowitUp.ogg",db+0,1};
+	duration=186.6;
+	musicClass="CommandConquer_Tiberium";
+};
+class Crush
+{	
+	name="[Tiberium Wars] Crush";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\Crush.ogg",db+0,1};
+	duration=209.4;
+	musicClass="CommandConquer_Tiberium";
+};
+class HellMarch
+{	
+	name="[Tiberium Wars] Hell March";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\HellMarch.ogg",db+0,1};
+	duration=374.4;
+	musicClass="CommandConquer_Tiberium";
+};
+class MilitantForce
+{	
+	name="[Tiberium Wars] Militant Force";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\MilitantForce.ogg",db+0,1};
+	duration=90;
+	musicClass="CommandConquer_Tiberium";
+};
+class Mutants
+{	
+	name="[Tiberium Wars] Mutants";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\Mutants.ogg",db+0,1};
+	duration=249;
+	musicClass="CommandConquer_Tiberium";
+};
+class PrepareforBattle
+{	
+	name="[Tiberium Wars] Prepare for Battle";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\PrepareforBattle.ogg",db+0,1};
+	duration=197.4;
+	musicClass="CommandConquer_Tiberium";
+};
+class RainintheNight
+{	
+	name="[Tiberium Wars] Rain in the Night";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\RainintheNight.ogg",db+0,1};
+	duration=140.4;
+	musicClass="CommandConquer_Tiberium";
+};
+class Target
+{	
+	name="[Tiberium Wars] Target";
+	sound[]={"\Command\Command_Audio\Music\CommandConquer\Tiberium\Target.ogg",db+0,1};
+	duration=151.2;
+	musicClass="CommandConquer_Tiberium";
+};
